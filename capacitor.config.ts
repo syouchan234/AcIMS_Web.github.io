b51c66d1e58253493bb1f87cd431dcbd9e34f435
Syouchan <syou5963@outlook.jp>
2026-08-14 08:24:59 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AcIMSWeb',
+  webDir: 'dist'
+};
+
+export default config;
