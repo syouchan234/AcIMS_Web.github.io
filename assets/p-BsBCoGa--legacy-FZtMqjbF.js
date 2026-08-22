66cda462eb92a5d4a399b08d215e8a3196815143
Syouchan <syou5963@outlook.jp>
2026-08-22 21:46:44 +0900

    Updates

--- assets/p-BsBCoGa--legacy-FZtMqjbF.js
+++ assets/p-BsBCoGa--legacy-FZtMqjbF.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BnygUObB.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.U,i=e.r}],execute:function(){
+System.register(["./index-legacy-CaiVFTEZ.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.U,i=e.r}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
