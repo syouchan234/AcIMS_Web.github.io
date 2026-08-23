a993fc7f0e365fe6708f25bf7c980d68e37b0ae4
Syouchan <syou5963@outlook.jp>
2026-08-23 16:44:59 +0900

    Updates

--- assets/p-DWkAgceV-legacy-nmf_45vq.js
+++ assets/p-DWkAgceV-legacy-nmf_45vq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CaEnYpJA.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.U}],execute:function(){
+System.register(["./index-legacy-Cy80OFkL.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.U}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
