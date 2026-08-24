42c5a9e80fad126b78ee58cac4c1157a31a3812e
Syouchan <syou5963@outlook.jp>
2026-08-24 19:46:46 +0900

    Updates

--- assets/p-gAg0kXZp-legacy-CKkLcNZP.js
+++ assets/p-gAg0kXZp-legacy-CKkLcNZP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CostO_33.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
+System.register(["./index-legacy--TqMsd10.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
