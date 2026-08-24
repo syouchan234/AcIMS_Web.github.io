6d22c9ae046672c851771bd47ede07c682ff7b32
Syouchan <syou5963@outlook.jp>
2026-08-24 19:56:37 +0900

    Updates

--- assets/p-BsBCoGa--legacy-60kcLBn0.js
+++ assets/p-BsBCoGa--legacy-60kcLBn0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy--TqMsd10.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.U,i=e.r}],execute:function(){
+System.register(["./index-legacy-DGczFQNE.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.U,i=e.r}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
--- assets/p-R8zmRi4c-legacy-BSoZhcwO.js
+++ assets/p-R8zmRi4c-legacy-BSoZhcwO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy--TqMsd10.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.H,r=e.l,s=e.e,o=e.P,i=e.c}],execute:function(){
+System.register(["./index-legacy-DGczFQNE.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.H,r=e.l,s=e.e,o=e.P,i=e.c}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
