cad571b71e0f9314e5089c0fc62d0a212c750792
Syouchan <syou5963@outlook.jp>
2026-08-24 20:44:11 +0900

    Updates

--- assets/p-R8zmRi4c-legacy-3IjKd3ai.js
+++ assets/p-R8zmRi4c-legacy-3IjKd3ai.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dxss5FsY.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.H,r=e.l,s=e.e,o=e.P,i=e.c}],execute:function(){
+System.register(["./index-legacy-xtyNavec.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.H,r=e.l,s=e.e,o=e.P,i=e.c}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
