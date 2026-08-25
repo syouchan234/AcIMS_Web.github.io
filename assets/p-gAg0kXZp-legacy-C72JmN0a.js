41374e0ec2a87d0ac841d0111e8447450ce36c42
Syouchan <syou5963@outlook.jp>
2026-08-25 19:13:40 +0900

    Updates

--- assets/p-gAg0kXZp-legacy-C72JmN0a.js
+++ assets/p-gAg0kXZp-legacy-C72JmN0a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BqfbBKyE.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
+System.register(["./index-legacy-BoXxloDN.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
